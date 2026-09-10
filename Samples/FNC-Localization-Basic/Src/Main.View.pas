c976154f7dc90fdd4cd482e0eb49c2b5b9eb9b65
César Cardoso <contato@code4delphi.com.br>
2026-09-10 16:48:56 -0300

    2026-09-10-1648

--- Samples/FNC-Localization-Basic/Src/Main.View.pas
+++ Samples/FNC-Localization-Basic/Src/Main.View.pas
@@ -3,10 +3,25 @@ unit Main.View;
 interface
 
 uses
-  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
-  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.StdCtrls, FMX.Controls.Presentation,
-  TMS.TMSFNCLocalizationCollector, TMS.TMSFNCLocalizationLocalizer, FMX.ListBox, FMX.TMSFNCLocalizationComboBox,
-  FMX.Menus;
+  System.SysUtils,
+  System.Types,
+  System.UITypes,
+  System.Classes,
+  System.Variants,
+  FMX.Types,
+  FMX.Controls,
+  FMX.Forms,
+  FMX.Graphics,
+  FMX.Dialogs,
+  FMX.StdCtrls,
+  FMX.Controls.Presentation,
+  TMS.TMSFNCLocalizationCollector,
+  TMS.TMSFNCLocalizationLocalizer,
+  FMX.ListBox,
+  FMX.TMSFNCLocalizationComboBox,
+  FMX.Menus,
+  FMX.TMSFNCCustomComponent,
+  FMX.TMSFNCLocalizationEditor;
 
 type
   TMainView = class(TForm)
@@ -16,19 +31,22 @@ type
     MenuItem1: TMenuItem;
     MenuItem2: TMenuItem;
     MenuItem3: TMenuItem;
-    Panel1: TPanel;
+    pnBack: TPanel;
     lbWelcome: TLabel;
     btnRegisterCustomer: TButton;
     Button1: TButton;
     TMSFNCLocalizationComboBox1: TTMSFNCLocalizationComboBox;
     btnOpenEditor: TButton;
     lbSelectLanguage: TLabel;
+    TMSFNCLocalizationEditor1: TTMSFNCLocalizationEditor;
     procedure FormCreate(Sender: TObject);
     procedure Button1Click(Sender: TObject);
+    procedure btnOpenEditorClick(Sender: TObject);
+    procedure btnRegisterCustomerClick(Sender: TObject);
   private
-    { Private declarations }
+
   public
-    { Public declarations }
+
   end;
 
 var
@@ -43,9 +61,19 @@ begin
   TMSFNCLocalizationLocalizer1.PerformLocalization;
 end;
 
+procedure TMainView.btnRegisterCustomerClick(Sender: TObject);
+begin
+  ShowMessage('Click');
+end;
+
 procedure TMainView.Button1Click(Sender: TObject);
 begin
   TMSFNCLocalizationLocalizer1.TrySetLocale('pt-BR');
+end;
+
+procedure TMainView.btnOpenEditorClick(Sender: TObject);
+begin
+  TMSFNCLocalizationEditor1.Execute;
 end;
 
 end.
